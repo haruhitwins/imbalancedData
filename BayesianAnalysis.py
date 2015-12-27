0bc02516b6c1a89ab8c11aa08199471d91711d18
Haolin <zhl_moly@qq.com>
2015-12-27 20:41:49 +0800

    Just a backup
    Add slice sampling; experiments can be done with initial settings. So
    far the performance is not good, sigh...

--- BayesianAnalysis.py
+++ BayesianAnalysis.py
@@ -36,30 +36,55 @@ class JointDist(object):
         return self.loglikelihood(beta, xi) + \
                np.log(np.power(det, -0.5)) - 0.5 * beta.dot(inv).dot(beta).reshape(-1, 1) + \
                np.log(np.power(self.sigmaXi, -0.5)) - 1./(2*self.sigmaXi)*xi*xi
+               
+    def pdf(self, z):
+        return np.exp(self.logpdf(z))
 
 
-trainX, trainY, testX, testY = Util.readData("data/harberman.data")
+trainX, trainY, testX, testY = Util.readData("data/pima.data")
 D = trainX.shape[1]
 jd = JointDist(trainX, trainY)
-# shape (1, D)
 sigmaBeta = np.random.rand(D)
-# scala
 sigmaXi = np.random.rand()
 Q = []
+
+#beta = np.random.multivariate_normal([0.]*D, np.diag(sigmaBeta))
+#xi = np.array([np.sqrt(sigmaXi) * np.random.randn()])
+#init = np.concatenate((beta, xi))
+init = np.zeros(D + 1)
+#qsigma = 0.0005*np.eye(D+1)
+
+print "logpdf = ", jd.logpdf(init)
+print "pdf = ", jd.pdf(init)
+
 for i in xrange(50):
     jd.setSigmaBeta(sigmaBeta)
     jd.setSigmaXi(sigmaXi)
-#    beta = np.random.multivariate_normal([0.]*D, np.diag(sigmaBeta))
-#    xi = np.array([sigmaXi * np.random.randn()])
-#    init = np.concatenate((beta, xi))
-    init = np.random.randn(D+1)
-    samples = Sampling.MH_Sampling(init, jd.logpdf, 5000, np.diag(np.random.rand(D+1)))#5*np.eye(D+1))
-    samples = samples[1000:]
+    qsigma = np.diag(0.000005 * np.random.rand(D+1))
+    samples = Sampling.MH_Sampling(init, jd.logpdf, 5000, 1000, 20, qsigma)
+    #samples = Sampling.Slice_Sampling_Multi(init, jd.pdf, 1000, 0.01, 10)
     sigma = (samples**2).sum(axis=0)/float(samples.shape[0])
     sigmaBeta, sigmaXi = sigma[:-1], sigma[-1]    
     q = np.mean([jd.logpdf(s) for s in samples])
-    print "Q = ", q
+    #init = np.mean(samples, axis=0)
+    #print "Q = ", q
     Q.append(q)
+#    if (samples == samples[0]).all():
+#        break
         
 plt.plot(Q)
 plt.show()    
+
+predY = np.zeros(testY.size).reshape(-1, 1)
+for s in samples:
+    beta, xi = s[:-1], s[-1]
+    v = testX.dot(beta.reshape(-1, 1))
+    GEVFunc.clip(xi, v)
+    predY += GEVFunc.inverseLink(xi, v)
+predY /= samples.shape[0]
+predY = predY.flatten()
+print Util.evaluate(predY, testY)
+print predY
+
+
+
--- Sampling.py
+++ Sampling.py
@@ -5,6 +5,7 @@ Created on Mon Dec 21 11:19:26 2015
 @author: Haolin
 """
 
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,7 +18,7 @@ def log_pdf(x):
 def pdf(x):
     return 1./3 * (gaussian(x, 1, 0.1) + gaussian(x, 2, 0.05) + gaussian(x, 3, 0.1))
 
-def MH_Sampling(init, log_pdf, iters, sigma):
+def MH_Sampling(init, log_pdf, iters, burnin, Mstep, sigma):
     D = init.size
     samples = np.zeros((iters, D))
     state = init
@@ -32,12 +33,106 @@ def MH_Sampling(init, log_pdf, iters, sigma):
             acceptance += 1
         samples[i, :] = state
     print "Accept rate: %f" % (acceptance/float(iters))
+    samples = samples[burnin:]
+    index = np.array(range(samples.shape[0]))
+    return samples[index % Mstep == 0]
+
+def steppingOut(f, x, y, w, m):
+    u, v = np.random.rand(), np.random.rand()
+    L = x - w * u
+    R = L + w
+    j = math.floor(m * v)
+    k = m - 1 - j
+    while j > 0 and y < f(L):
+        L -= w
+        j -= 1
+    while k > 0 and y < f(R):
+        R += w
+        k -= 1
+    return (L, R)
+    
+def shrinkage(f, x, y, (L, R)):
+    l, r = L, R
+    while 1:
+        u = np.random.rand()
+        sample = l + u * (r - l)
+        if y < f(sample):
+            return sample
+        if sample < x:
+            l = sample
+        else:
+            r = sample
+    
+def Slice_Sampling(x, pdf, iters, w, m):
+    samples = np.zeros(iters)
+    for i in xrange(iters):
+        y = np.random.rand() * pdf(x)
+        (L, R) = steppingOut(pdf, x, y, w, m)
+        s = shrinkage(pdf, x, y, (L, R))
+        samples[i] = s
+        x = s
     return samples
 
+def steppingOut_multi(pdf, z, index, y, w, m):
+    u, v = np.random.rand(), np.random.rand()
+    x = z[index]
+    L = x - w * u
+    R = L + w
+    j = math.floor(m * v)
+    k = m - 1 - j
+    tmp = z.copy()
+    tmp[index] = L
+    while j > 0 and y < pdf(tmp):
+        L -= w
+        tmp[index] = L
+        j -= 1
+    tmp[index] = R
+    while k > 0 and y < pdf(tmp):
+        R += w
+        tmp[index] = R
+        k -= 1
+    return (L, R)
+
+def shrinkage_multi(pdf, z, index, y, (L, R)):
+    l, r = L, R
+    tmp = z.copy()
+#    print "y = ", y
+#    tmp[index] = R
+#    print "pdf(R) = ", pdf(tmp)
+#    tmp[index] = L
+#    print "pdf(L) = ", pdf(tmp)
+    while 1:
+        u = np.random.rand()
+        sample = l + u * (r - l)
+        #print sample, l, r, r-l
+        tmp[index] = sample
+        if y < pdf(tmp):
+            return sample
+        if sample < z[index]:
+            l = sample
+        else:
+            r = sample
+    
+def Slice_Sampling_Multi(init, pdf, iters, w, m):
+    D = init.size
+    state = init
+    samples = np.zeros((iters, D))
+    for i in xrange(iters):
+        #sample = np.zeros(D)
+        for j in xrange(D):
+            y = np.random.rand() * pdf(state)
+            (L, R) = steppingOut_multi(pdf, state, j, y, w, m)
+            s = shrinkage_multi(pdf, state, j, y, (L, R))
+            #sample[j] = s
+            state[j] = s
+        #samples[i] = sample
+        samples[i] = state
+    return samples
+   
 if __name__ == "__main__":
-    init = np.array([np.random.rand()])
-    s = MH_Sampling(init, log_pdf, 5000, 2 * np.eye(1))
-    s = s[500:] #Ommit burn-in
+#    init = np.array([np.random.rand()])
+#    s = MH_Sampling(init, log_pdf, 5000, 1000, 10, 2 * np.eye(1))
+    s = Slice_Sampling(np.random.rand(), pdf, 3000, 0.1, 10)
     n, bins, patches = plt.hist(s, 50, normed=1, facecolor='g', alpha=0.75)
     plt.axis([-1, 5, 0, 1])
     x = np.linspace(0, 4, 100)
